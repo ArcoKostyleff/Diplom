553d491e9643775b43bf5574a3a6a5bc567315bf
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2024-06-19 09:33:17 +0300

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,58 @@
+sudo apt install python3-pip
+sudo pip3 install pyserial
+sudo apt install lm-sensors
+sudo apt-get install python-rpi.gpio python3-rpi.gpio
+sudo raspi-config
+sudo pip install RPi.GPIO
+sudo pip install pyLoRa
+sudo raspi-config
+sudo raspi-config
+ls -lh /dev | grep serial
+sudo apt-get install python-serial
+sudo apt-get install python3-rpi.gpio
+sudo apt install python3-pip
+sudo pip3 install pyserial
+pinout
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+armbian.list
+rsync -av rsync://rsync.armbian.com/
+apt-get update 
+sudo apt-get update
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+nano /etc/apt/sources.list.d/armbian.list
+sudo nano /etc/apt/sources.list.d/raspi.list
+sudo apt-get update
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install libasound2-dev
+sudo apt-get install libasound2
+sudo apt-get install libasound2-dev
+sudo apt-get install libasound19
+sudo apt-get install portaudio19
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install libasound-dev
+sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install python3-pyaudio
+sudo python lora.py
+sudo pip3 install pyaudio
+sudo python lora.py
+sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install python-pyaudio
+sudo python lora.py
+sudo nano lora.py
+sudo python detector.py
+sudo nano detector.py
+sudo python detector.py
+sudo nano detector.py
+sudo python detector.py
+edge-impulse-linux --clean
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt install -y gcc g++ make build-essential nodejs sox gstreamer1.0-tools gstreamer1
+sudo apt update
+sudo apt-get install libatlas-base-dev libportaudio0 libportaudio2 libportaudiocpp0 po
+sudo apt-get install libatlas-base-dev libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+pip3 install edge_impulse_linux -i https://pypi.python.org/simple
+sudo python detector.py
+pip3 install edge_impulse_linux -i https://pypi.python.org/simple
+pip3 install pyaudio edge_impulse_linux
+pip3 install edge_impulse_linux -i https://pypi.python.org/simple
+edge-impulse-linux-runner --download modelfile.eim
